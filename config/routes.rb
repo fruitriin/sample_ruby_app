a2581eac07e4add0077ff78eac105a33232c04ce
Riin Fruit <nassii74@gmail.com>
2020-03-01 16:22:39 +0900

    feat: password reset

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   resource :account_activations, only: [:edit]
   resources :users
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
